907bbbef06170a6bda6d6d85f76001b7d7a7f5ad
RealMrHex <RealMrHex@gmail.com>
2024-06-23 03:18:08 +0330

    testing actions

--- tests/Feature/Livewire/TaskListTest.php
+++ tests/Feature/Livewire/TaskListTest.php
@@ -3,16 +3,44 @@
 namespace Tests\Feature\Livewire;
 
 use App\Livewire\TaskList;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
 use Tests\TestCase;
 
 class TaskListTest extends TestCase
 {
+    protected string $title = 'Go to gym';
+    protected string $description = 'Nothing is more energiser that going to the gym';
+
+    protected function livewire()
+    {
+        return Livewire::test(TaskList::class);
+    }
+
     public function test_renders_successfully()
     {
-        Livewire::test(TaskList::class)
-            ->assertStatus(200);
+        $this->livewire()->assertStatus(200);
+    }
+
+    public function test_title_and_description_set_successfully()
+    {
+        $this->livewire()
+            ->set('title', $this->title)
+            ->set('description', $this->description)
+            ->assertSet('title', $this->title)
+            ->assertSet('description', $this->description);
+    }
+
+    public function test_task_can_be_added_to_list()
+    {
+        $this->livewire()
+            ->assertViewHas('tasks', function ($tasks) {
+                return count($tasks) === 0;
+            })
+            ->set('title', $this->title)
+            ->set('description', $this->description)
+            ->call('add')
+            ->assertViewHas('tasks', function ($tasks) {
+                return count($tasks) === 1;
+            });
     }
 }
